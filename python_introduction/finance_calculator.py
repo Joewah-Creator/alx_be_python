955270e756bdb0895f20c963ef93dd652120d9c1
josphat wahome <wahomejosphatt27@gmail.com>
2025-09-13 14:45:10 +0300

    Added the recomended interaction values to finance_calculator.py

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -15,3 +15,19 @@ projected_savings = annual_savings + (annual_savings * 0.05)
 print(f"Your monthly savings are ${monthly_savings:.2f}.")
 print(f"Projected savings after one year, with interest, is: ${projected_savings:.2f}.")
 
+# finance_calculator.py
+
+# prompt user for inputs
+monthly_income = float(input("Enter your monthly income: "))
+monthly_expenses = float(input("Enter your total monthly expenses: "))
+
+# calculate monthly savings
+monthly_savings = monthly_income - monthly_expenses
+
+# calculate projected annual savings with 5% interest
+annual_savings = monthly_savings * 12
+projected_savings = annual_savings + (annual_savings * 0.05)
+
+# display results
+print(f"Your monthly savings are ${monthly_savings:.2f}.")
+print(f"Projected savings after one year, with interest, is: ${projected_savings:.2f}.")
