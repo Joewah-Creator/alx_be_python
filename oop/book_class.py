a5228d4a331b9187cfc13ca5363e24399d257461
josphat wahome <wahomejosphatt27@gmail.com>
2025-10-12 12:59:32 +0300

    Add code to book_class.py

---
+++ oop/book_class.py
@@ -0,0 +1,16 @@
+# oop/book_class.py
+
+class Book:
+    def __init__(self, title, author, year): # Constructor: Initializes a Book instance with title, author, and year.
+        self.title = title
+        self.author = author
+        self.year = year
+
+    def __del__(self): # Destructor: Prints a message when the book object is deleted.
+        print(f"Deleting {self.title}")
+
+    def __str__(self): # String representation: Used when printing the object.
+        return f"{self.title} by {self.author}, published in {self.year}"
+
+    def __repr__(self): #Official representation: Used for debugging and recreating the object.
+        return f"Book('{self.title}', '{self.author}', {self.year})"
